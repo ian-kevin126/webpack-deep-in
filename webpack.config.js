ee5b89205b96902c567d6eab76581ad332aef4d6
ian-kevin126 <ian_kevin126@126.com>
2021-08-06 10:23:29 +0800

    feat: create loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   /*
@@ -7,17 +7,17 @@ module.exports = {
   development: cheap-module-eval-source-map
   production: cheap-module-source-map
   * */
-  devtool: "eval-cheap-module-source-map",
+  devtool: 'eval-cheap-module-source-map',
   /*
   mode: 指定打包的模式, 模式有两种
   一种是开发模式(development): 不会对打包的JS代码进行压缩
   还有一种就是上线(生产)模式(production): 会对打包的JS代码进行压缩
   * */
-  mode: "development", // "production" | "development"
+  mode: 'development', // "production" | "development"
   /*
   entry: 指定需要打包的文件
   * */
-  entry: "./src/index.js",
+  entry: './src/index.js',
   /*
   output: 指定打包之后的文件输出的路径和输出的文件名称
   * */
@@ -25,11 +25,11 @@ module.exports = {
     /*
     filename: 指定打包之后的JS文件的名称
     * */
-    filename: "js/[name].js",
+    filename: 'js/[name].js',
     /*
     path: 指定打包之后的文件存储到什么地方
     * */
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, 'dist')
   },
   /*
   module: 告诉webpack如何处理webpack不能够识别的文件
@@ -37,14 +37,23 @@ module.exports = {
   module: {
     rules: [
       // 打包JS规则
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/, // 告诉webpack不处理哪一个文件夹
+      //   loader: "babel-loader",
+      //   options: {
+      //     "presets": [["@babel/preset-env"]],
+      //   }
+      // },
       {
         test: /\.js$/,
-        exclude: /node_modules/, // 告诉webpack不处理哪一个文件夹
-        loader: "babel-loader",
-        options: {
-          "presets": [["@babel/preset-env"]],
-        }
-      },
+        use: [{
+          loader: path.resolve(__dirname, 'loader/ReplaceLoader.js'),
+          options: {
+            name: 'lnj'
+          }
+        }]
+      }
     ]
   },
   /*
@@ -53,7 +62,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       // 指定打包的模板, 如果不指定会自动生成一个空的
-      template: "./index.html",
+      template: './index.html',
     }),
   ]
-};
+}
